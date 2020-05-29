c97f45aa99570f4b58f2ed17df5b287acf4cb95c
williamwjd <william.jose.wjd@gmail.com>
2020-05-29 15:03:06 -0300

    ajustando model category

--- backend/src/app/models/Category.js
+++ backend/src/app/models/Category.js
@@ -4,7 +4,8 @@ class Category extends Model{
     static init(sequelize){
         super.init({
             description: Sequelize.STRING,
-            type: Sequelize.ENUM('RECEITAS', 'DESPESAS')
+            type: Sequelize.ENUM('RECEITAS', 'DESPESAS'),
+            color: Sequelize.STRING
         },{
             sequelize,
             tableName:'categorys'
