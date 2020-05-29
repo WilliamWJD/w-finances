df513f7718d3b976d882ee19560b3ba52d79a175
williamwjd <william.jose.wjd@gmail.com>
2020-05-29 15:07:46 -0300

    seprando o botão de menu em um componente

---
+++ mobile/src/components/FabButton/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import ActionButton from 'react-native-action-button';
+import { MaterialIcons } from '@expo/vector-icons'
+
+const FabButton = () => {
+    return (
+        <ActionButton buttonColor="#6E21D1">
+            <ActionButton.Item buttonColor='#e74c3c' title="Contas" onPress={() => console.log("notes tapped!")}>
+                <MaterialIcons name="account-balance" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+            <ActionButton.Item buttonColor='#3498db' title="Categorias" onPress={() => { }}>
+                <MaterialIcons name="category" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+            <ActionButton.Item buttonColor='#27ae60' title="Receitas" onPress={() => { }}>
+                <MaterialIcons name="call-made" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+            <ActionButton.Item buttonColor='#c0392b' title="Despesas" onPress={() => { }}>
+                <MaterialIcons name="call-received" style={styles.actionButtonIcon} />
+            </ActionButton.Item>
+        </ActionButton>
+    )
+}
+
+const styles = StyleSheet.create({
+    actionButtonIcon: {
+        fontSize: 20,
+        height: 22,
+        color: 'white'
+    },
+})
+
+export default FabButton
--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import ActionButton from 'react-native-action-button';
-import { MaterialIcons } from '@expo/vector-icons'
 
 import Header from '../../components/Header'
 import PanelTypes from '../../components/PanelTypes'
 import Transactions from '../../components/Transactions'
+import FabButton from '../../components/FabButton'
 
 const Home = () => {
     return (
@@ -13,21 +12,7 @@ const Home = () => {
             <Header />
             <PanelTypes />
             <Transactions />
-            
-            <ActionButton buttonColor="#6E21D1">
-                <ActionButton.Item buttonColor='#e74c3c' title="Contas" onPress={() => console.log("notes tapped!")}>
-                    <MaterialIcons name="account-balance" style={styles.actionButtonIcon}/>
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#3498db' title="Categorias" onPress={() => { }}>
-                    <MaterialIcons name="category" style={styles.actionButtonIcon}/>
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#27ae60' title="Receitas" onPress={() => { }}>
-                    <MaterialIcons name="call-made" style={styles.actionButtonIcon}/>
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#c0392b' title="Despesas" onPress={() => { }}>
-                    <MaterialIcons name="call-received" style={styles.actionButtonIcon}/>
-                </ActionButton.Item>
-            </ActionButton>
+            <FabButton />
         </View>
     )
 }
@@ -38,12 +23,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 22,
-    },
-
-    actionButtonIcon: {
-        fontSize: 20,
-        height: 22,
-        color: 'white'
     },
 })
 
